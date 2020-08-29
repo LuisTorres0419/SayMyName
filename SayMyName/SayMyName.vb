510c833117666697dbf48d5d6a7e106e0a1113fb
luis torres <tl224627@gmail.com>
2020-08-28 19:20:18 -0600

    Add project files.

---
+++ SayMyName/SayMyName.vb
@@ -0,0 +1,20 @@
+﻿'Luis Torres
+'RCET0265
+'Fall 2020
+'Say My Name
+'
+Option Strict On
+Option Explicit On
+
+
+Module SayMyName
+
+    Sub Main()
+        Dim userInput As String
+        Console.WriteLine("Hey!! What's your name stranger?")
+        userInput = Console.ReadLine()
+        Console.WriteLine("Really your name is " & userInput & " that was my mothers name. my name is computer. Nice to meet you!!!!!")
+        Console.ReadLine()
+    End Sub
+
+End Module
